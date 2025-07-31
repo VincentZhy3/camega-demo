224c5e32723ee1af599a1dd1330dabd780d63f95
Vincent Zhang <vincentzhang@bluedrop.com>
2025-07-31 13:10:26 -0230

    revert

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3jMCEirmbpKmE8_jxeTAtXIYfCca-TBs", // 替换为你的 Firebase API 密钥（不是 GitHub Token！）
+    authDomain: "camega-user-management.firebaseapp.com",
+    projectId: "camega-user-management",
+    storageBucket: "camega-user-management.firebasestorage.app",
+    messagingSenderId: "54421041663",
+    appId: "1:54421041663:web:b0e461c4c68d3d248ef7ca",
+    measurementId: "G-4C9TRTLH6X"
+}; 
