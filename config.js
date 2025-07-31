61eb0e80e9a72ea37be444c87e91543b0b8c452a
VincentZhy3 <VincentZhy3@users.noreply.github.com>
2025-07-31 15:33:44 +0000

    deploy: 759f8a5799dadc1ed148cbf5aec0043b09c21bef

---
+++ config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3jMCEirmbpKmE8_jxeTAtXIYfCca-TBs",
+    authDomain: "camega-user-management.firebaseapp.com",
+    projectId: "camega-user-management",
+    storageBucket: "camega-user-management.firebasestorage.app",
+    messagingSenderId: "54421041663",
+    appId: "1:54421041663:web:b0e461c4c68d3d248ef7ca",
+    measurementId: "G-4C9TRTLH6X"
+};
